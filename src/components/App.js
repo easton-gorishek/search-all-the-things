6ba310b28f2367025139d114124d87bdbd617ab2
Easton Gorishek <ieaston26@gmail.com>
2018-08-15 17:39:08 -0700

    App component connected

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>REACT TIME</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
